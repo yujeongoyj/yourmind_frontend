f21bb2c64d6c9ddacc7f8f06e0f9d8c0fe2d6d1a
Kimsumin123 <tntn211@naver.com>
2023-05-11 16:47:03 +0900

    pages

---
+++ src/pages/Start.jsx
@@ -0,0 +1,24 @@
+// import '../css/Login.css';
+// import {Route} from "react-router-dom";
+
+const Start = () => {
+    return(
+        <div>
+            {/* <div id="LogInImg">
+                <img id="LogInImg" src={그림1}/>
+            </div> */}
+            <form id="LogInput">
+                아이디 : <input id="Id" type="text" placeholder="이메일을 입력하세요."/> <br/>
+                비밀번호 : <input id="Password" type="password" placeholder="비밀번호를 입력하세요."/><br/>
+                <button id="Button" type="submit">로그인</button>
+                <br/>
+                <button id="FindId" type="submit">아이디 찾기</button>
+                <button id="FindPassword" type="submit">비밀번호 찾기</button>
+                <button id="Member" type="submit">회원가입 하기</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Start;
