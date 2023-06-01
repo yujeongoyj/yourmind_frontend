e4adb94b856d65c542cd553bc2d1c21067186e01
Kimsumin123 <tntn211@naver.com>
2023-06-01 19:35:03 +0900

    스프링데이터 가져오기

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ import JoinOk from "./JoinOk";
 import Main from "./Main";
 import Login from "./Login";
 import Character from "./character";
+import MyComponent from "./pages/Start";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <MyComponent/>
         </BrowserRouter>
     </React.StrictMode>
 );
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,28 +1,34 @@
-// import '../css/Login.css';
-import {Route} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import './Start.css';
+function MyComponent() {
+  const [data, setData] = useState(null);
 
-const Start = () => {
-    return(
-        <div id box1>
-            {/* <div id="LogInImg">
-                <img id="LogInImg" src={그림1}/>
-            </div> */}
-            <form id="LogInput">
-                아이디 : <input id="Id" type="text" placeholder="이메일을 입력하세요."/> <br/>
-                비밀번호 : <input id="Password" type="password" placeholder="비밀번호를 입력하세요."/><br/>
-                <button id="Button" type="submit">로그인</button>
-                <br/>
-                <button id="FindId" type="submit">아이디 찾기</button>
-                <button id="FindPassword" type="submit">비밀번호 찾기</button>
-                <button id="Member" type="submit">회원가입 하기</button>
-            </form>
-            
-        </div>
-    )
+  useEffect(() => {
+    // API 요청 수행
+    axios.get('http://localhost:8080/api/users')
+      .then(response => {
+        // 데이터 처리
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log('Error:', error.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>console.error();</p>
+      )}
+    </div>
+  );
 }
 
-
-export default Start;
-
+export default MyComponent;
